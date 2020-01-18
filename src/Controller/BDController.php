136e7b0c793dcec1590c0605294a44859184b695
Arthur Pralon <arthur.pralon@wanadoo.fr>
2020-01-18 12:53:41 +0100

    reset user entity + auth components

--- src/Controller/BDController.php
+++ src/Controller/BDController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/back-office")
+ * @Route("/admin/back-office")
  */
 class BDController extends AbstractController
 {
